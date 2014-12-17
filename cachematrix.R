22f824e7045b7815e6425573093b23b1229e9e33
Sergey Reznick <Sergey.Reznick@gmail.com>
2014-12-18 00:10:48 +0400

    Implemented makeCacheMatrix and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions for cached computation of inverted matrix
+## It is assumed that matrix is invertible
 
-## Write a short comment describing this function
-
+## Create object representing matrix and its cached 
+## inverted peer
 makeCacheMatrix <- function(x = matrix()) {
+    inverted <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inv) inverted <<- inv
+    getinv <- function() inverted
+    list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 
 }
 
 
-## Write a short comment describing this function
+## Uses instance created by makeCacheMatrix
+## to obtain (calculating first time and using cached value next times)
+## inverted matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()
+    if(is.null(inv)) {
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setinv(inv)
+    } else {
+        message("getting cached data")
+    }
+    inv
 }
